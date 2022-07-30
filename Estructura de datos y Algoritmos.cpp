e444daeb9ca9d2d3ec2eeb5571623940c69e963a
Luco97 <renatoluco@gmail.com>
2022-07-30 18:15:00 -0400

    UPDATE se agregan otros archivos para dejar main limpio con resultado de tareas

--- Estructura de datos y Algoritmos.cpp
+++ Estructura de datos y Algoritmos.cpp
@@ -2,21 +2,9 @@
 #include <vector>
 
 //Headers
-#include "merge-sort.h"
+#include "mergeTest.h"
 
 int main()
 {
-	//std::vector<int> test = { 1 , 4 , 2 , 5 , 10 , 2 };
-	std::vector<int> test = { 3, 5, 6, 1, 2, 10, 9, 7, 4 };
-	for (int i : test)
-	{
-		std::cout << i << ", ";
-	}
-	std::cout << "\n";
-	MergeSort(test, 0, test.size() - 1);
-	for (int i : test)
-	{
-		std::cout << i << ", ";
-	}
-	return 1;
+	mergeTest();
 }
