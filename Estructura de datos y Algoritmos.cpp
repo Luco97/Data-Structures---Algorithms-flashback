9943e51177c8ffdd0700e5cafb0593e510ef365b
Luco97 <renatoluco@gmail.com>
2022-07-29 22:49:19 -0400

    UPDATE prueba merge-sort

--- Estructura de datos y Algoritmos.cpp
+++ Estructura de datos y Algoritmos.cpp
@@ -1,19 +1,22 @@
-// Estructura de datos y Algoritmos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
+#include <vector>
+
+//Headers
+#include "merge-sort.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//std::vector<int> test = { 1 , 4 , 2 , 5 , 10 , 2 };
+	std::vector<int> test = { 3, 5, 6, 1, 2, 10, 9, 7, 4 };
+	for (int i : test)
+	{
+		std::cout << i << ", ";
+	}
+	std::cout << "\n";
+	MergeSort(test, 0, test.size() - 1);
+	for (int i : test)
+	{
+		std::cout << i << ", ";
+	}
+	return 1;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
